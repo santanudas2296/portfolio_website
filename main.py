8ec31132043ceb269d5d137fc9016050a5044a77
Santanu2296 <santanudas229933+github@gmail.com>
2024-07-18 12:51:05 +0530

    added all app titles

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import streamlit as st
+import pandas as pd
 
 st.set_page_config(layout='wide')
 
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("images/photo.png")
+    st.image("images/photo.jpg",width=600)
 
 with col2:
     st.title("Santanu Das")
@@ -14,7 +15,23 @@ with col2:
     """
     st.info(content)
 
-text1 = """
-Below you can find some of the apps I have built in python
+content1 = """
+Below you can find some of the apps I have built in python.
+Feel free to contact me!
 """
-st.text(text1)
+st.write(content1)
+
+col3, col4 = st.columns(2)
+
+df = pd.read_csv("data.csv", sep=";")
+
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
+
+
+

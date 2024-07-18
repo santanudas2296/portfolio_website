a444b0753fc21648a5eedb511a5eda7136af477f
Santanu2296 <santanudas229933+github@gmail.com>
2024-07-18 16:49:51 +0530

    secured password

--- send_email.py
+++ send_email.py
@@ -1,5 +1,6 @@
 import smtplib
 import ssl
+import os
 
 
 def send_email(message):
@@ -7,7 +8,7 @@ def send_email(message):
     port = 465
 
     username = "santanudas229933@gmail.com"
-    password = "zqmndmnaqmiktlmj"
+    password = os.getenv('PASSWORD')
 
     context = ssl.create_default_context()
 
